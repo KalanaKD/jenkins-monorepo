d3de4288da9b2236d79b7d5707984b0c1700a45d
kalana <kalanakd471@gmail.com>
2026-09-11 15:05:32 +0530

    Bump version to 1.0.2 in package.json and add getDummyData function in index.js

--- packages/lib-b/src/index.js
+++ packages/lib-b/src/index.js
@@ -2,5 +2,13 @@ function getName() {
   return '@monorepo-test/lib-b';
 }
 
+function getDummyData() {
+  return {
+    message: 'Hello from lib-b',
+    version: '1.0.0',
+    buildTest: true
+  };
+}
+
 module.exports = { getName };
 // test
