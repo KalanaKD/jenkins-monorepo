c23f65cc50e4351823262e8e90483aef4c2855b6
kalana <kalanakd471@gmail.com>
2026-09-11 09:07:49 +0530

    Add getDummyData function to return sample data for testing

--- packages/lib-a/src/index.js
+++ packages/lib-a/src/index.js
@@ -2,4 +2,12 @@ function getName() {
   return '@monorepo-test/lib-a';
 }
 
+function getDummyData() {
+  return {
+    message: 'Hello from lib-a',
+    version: '1.0.1',
+    buildTest: true
+  };
+}
+
 module.exports = { getName };
